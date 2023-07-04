4c449095017d753209903fcea01fb7e573c00d83
lavanya <lavanyabogavarapu22@gmail.com>
2023-07-04 11:33:20 +0000

    change in code commit-2

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const element = (
         src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
         className="profile-image"
       />
-      <p className="profile-name"> Kiran V</p>
+      <h1 className="profile-name"> Kiran V</h1>
       <p className="paragraph">
-        {" "}
         Vishnu Institute of Education and Technology Bhimavaram{" "}
       </p>
       <img
